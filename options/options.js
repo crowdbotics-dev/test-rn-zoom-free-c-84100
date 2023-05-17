b821b8f5cd7b144fa8c8f20840cac6540518adce
Crowdbotics <engineering@crowdbotics.com>
2023-05-17 15:02:52 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "test-rn-zoom-free-c-84100",
+  url: "https://test-rn-zoom-free-c-84100.botics.co",
+  api: "https://test-rn-zoom-free-c-84100.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
